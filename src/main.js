dfa1121ca397baa690e139c5df33b176b5233c8d
PestoP <f1zanard@enib.fr>
2016-11-10 19:23:44 +0100

    Ajout de vue resource, vuex et vue router dans l'app

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import vueRouter from 'vue-router'
 
 // Semantic-UI css librairy
 import '../semantic/dist/semantic.css'
+
+Vue.use(Vuex)
+Vue.use(VueResource)
+Vue.use(vueRouter)
 
 /* eslint-disable no-new */
 new Vue({

b92a83af7e85ac3831903704246fd4d0bc414e24
PestoP <f1zanard@enib.fr>
2016-11-13 19:33:15 +0100

    placement des boutons du menu

--- src/components/NavCircle.vue
+++ src/components/NavCircle.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="nav_circle">
+  <div>
+    <div class="nav_circle"></div>
+    <button class="circle_button first block"><i class="icon heart center_block"></i></button>
+    <button class="circle_button second block"><i class="icon book center_block"></i></button>
+    <button class="circle_button third block"><i class="icon mail center_block"></i></button>
   </div>
 </template>
 
@@ -21,7 +25,42 @@ $circle_diameter: 90vh;
   border: solid 1px black;
   top: 50vh - $circle_diameter/2;
   left: -(35/100)*$circle_diameter;
-  position: relative;
+  position: absolute;
+}
+
+//diamètre des boutons
+$button_diameter: 5vh;
+
+.circle_button {
+  height: $button_diameter;
+  width: $button_diameter;
+  border-radius: $button_diameter/2;
+  border: solid 1px black;
+  position: absolute;
+
+  &.first {
+    top: 25vh;
+    left: 5vh;
+  }
+
+  &.second {
+    top: 47vh;
+    left: 11vh;
+  }
+
+  &.third {
+    top: 70vh;
+    left: 5vh;
+  }
+}
+
+.block {
+    display: block;
+}
+
+.center_block {
+  margin-left: auto;
+  margin-right: auto;
 }
 
 </style>

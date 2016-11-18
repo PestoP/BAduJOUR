21a38d62b1af5d7889bff990bb09b0f6d66e90b6
PestoP <f1zanard@enib.fr>
2016-11-18 10:18:16 +0100

    Texte de la page Ba du jour (sans relation avec le back-end)

--- src/components/Ba.vue
+++ src/components/Ba.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-  	<span>BA du jour</span>
+    <h1>La bonne action du jour</h1>
+    <p>
+      <span>Dire bonjour à toutes les personnes qui croiseront ton regard</span>
+    </p>
+      <button>Ok, je la fait !!</button>
+    <p>
+      <span>Déjà 672 personnes ont décidé de rendre le monde meilleur aujourd'hui !</span>
+    </p>
   </div>
 </template>
 

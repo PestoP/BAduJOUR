0e15f743c3a4ee2e2d026a603e99828099a586e3
PestoP <f1zanard@enib.fr>
2016-11-18 10:51:30 +0100

    Positionnement des éléments de la page 'ba du jour'

--- src/components/Ba.vue
+++ src/components/Ba.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <h1>La bonne action du jour</h1>
-    <p>
+  <div class="ba_du_jour_container">
+    <h1 class="title">La bonne action du jour</h1>
+    <p class="ba_du_jour">
       <span>Dire bonjour à toutes les personnes qui croiseront ton regard</span>
-    </p>
-      <button>Ok, je la fait !!</button>
-    <p>
-      <span>Déjà 672 personnes ont décidé de rendre le monde meilleur aujourd'hui !</span>
+    </p><!-- 
+    <button class="do_it">Ok, je la fait !!</button> -->
+    <div class="ui labeled button do_it" tabindex="0">
+      <div class="ui green button">
+        <i class="heart icon"></i> Ok, je la fait !!
+      </div>
+    </div>
+    <p class="impact">
+      <span>Déjà <b>672 personnes</b> ont décidé de rendre le monde meilleur aujourd'hui !</span>
     </p>
   </div>
 </template>
@@ -17,4 +22,24 @@ export default { }
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+.ba_du_jour_container {
+  text-align: center;
+
+  .title {
+    padding-top: 15vh;
+  }
+
+  .ba_du_jour {
+    padding-top: 24vh;
+  }
+
+  .do_it {
+    margin-top: 20vh;
+  }
+
+  .impact {
+    padding-top: 3vh;
+  }
+}
 </style>

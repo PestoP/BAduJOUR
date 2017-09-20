88d3a331c19af741a3c784f8e2a5465fca2b7f97
Thomas Nusbaum <thomasnusbaum@mbp-thomas.home>
2017-09-20 21:34:39 +0200

    wip

--- src/components/Ba.vue
+++ src/components/Ba.vue
@@ -3,21 +3,38 @@
     <h1 class="title">La bonne action du jour</h1>
     <p class="ba_du_jour">
       <span>Dire bonjour à toutes les personnes qui croiseront ton regard</span>
-    </p><!-- 
-    <button class="do_it">Ok, je la fait !!</button> -->
-    <div class="ui labeled button do_it" tabindex="0">
-      <div class="ui green button">
+    </p>
+    <!-- <button class="do_it">Ok, je la fait !!</button> -->
+    <div class="ui labeled button do_it" tabindex="0" v-on:click="incrementCounter">
+      <div class="ui green button" >
         <i class="heart icon"></i> Ok, je la fait !!
       </div>
     </div>
     <p class="impact">
-      <span>Déjà <b>672 personnes</b> ont décidé de rendre le monde meilleur aujourd'hui !</span>
+      <span>Déjà <b>{{ nbClick }} personnes</b> ont décidé de rendre le monde meilleur aujourd'hui !</span>
     </p>
   </div>
 </template>
 
 <script>
-export default { }
+export default {
+  data () {
+    return {
+      nbClick: 123
+    }
+  },
+  methods: {
+    incrementCounter () {
+      const canIncrement = sessionStorage.getItem('hasDoneBA') /* eslint-disable */
+      if (canIncrement) {
+        sessionStorage.setItem('hasDoneBA', true) /* eslint-disable */
+        this.nbClick++
+      } else {
+        console.log('pd tu as déja cliqué')
+      }
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->

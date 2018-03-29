8e310e092ef1d6d934b1f429da56b63a5bd8197e
PestoP <zanardo.florian@gmail.com>
2018-03-29 22:03:25 +0200

    chore(font awesome): use font awesome for icon
    Semantic ui, only for icons => overkill

--- src/components/NavCircle.vue
+++ src/components/NavCircle.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="nav_circle"></div>
-    <router-link :to="{name: 'ba'}" tag="button" class="circle_button yellow_button first block"><i class="icon yellow_darken heart center_block"></i></router-link>
-    <router-link :to="{name: 'story'}" tag="button" class="circle_button green_button second block"><i class="icon green_darken book center_block"></i></router-link>
-    <router-link :to="{name: 'propose'}" tag="button"  class="circle_button blue_button third block"><i class="icon blue_darken mail center_block"></i></router-link>
+    <router-link :to="{name: 'ba'}" tag="button" class="circle_button yellow_button first block"><i class="fas fa-heart yellow_darken center_block"></i></router-link>
+    <router-link :to="{name: 'story'}" tag="button" class="circle_button green_button second block"><i class="fas fa-book green_darken center_block"></i></router-link>
+    <router-link :to="{name: 'propose'}" tag="button"  class="circle_button blue_button third block"><i class="fas fa-envelope blue_darken center_block"></i></router-link>
   </div>
 </template>
 
@@ -19,7 +19,7 @@ export default { }
   // paramètres pour la mixin demi-cercle
   $circle_diameter: 90vh;
   $IsLeftSide: false;
-  
+
   @include demi-circle($circle_diameter, $IsLeftSide);
   top: 50vh - $circle_diameter/2;
   left: -(35/100)*$circle_diameter;
@@ -37,7 +37,7 @@ $button_diameter: 5vh;
   position: absolute;
   cursor: pointer;
   outline: none;
-  
+
   i {
     font-size: 1.3rem!important;
   }
@@ -49,7 +49,7 @@ $button_diameter: 5vh;
     &:hover {
       border-color: $yellow_darken;
     }
-  } 
+  }
 
   &.green_button {
     background-color: $green;
@@ -67,7 +67,7 @@ $button_diameter: 5vh;
     &:hover {
       border-color: $blue_darken;
     }
-  } 
+  }
 
   &:active {
     transform: rotate(25deg);
